9c19f02b6c0cf20e98833266c5658859a8216797
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2020-05-15 10:32:05 -0400

    another 8kyu

---
+++ 8kyu_Even_or_Odd/even_or_odd.js
@@ -0,0 +1,18 @@
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+    return ( number % 2 == 0 ? "Even" : "Odd" )
+  }
+
+// Ecma 6 answer ^^^
+
+
+function even_or_odd(number) {
+    if (number % 2 === 0){
+    return "Even"
+    }else{
+    return "Odd"
+    }}
+
+
+// Basic Answer 
