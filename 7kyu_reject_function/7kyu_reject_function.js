29d70d33cf29bc51600901dd73eaecd7a1d233c8
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2020-06-18 08:32:11 -0400

    added 8kyu

--- 7kyu_reject_function/7kyu_reject_function.js
+++ 7kyu_reject_function/7kyu_reject_function.js
@@ -3,15 +3,11 @@
 // reject([1, 2, 3, 4, 5, 6], (n) => n % 2 === 0)  =>  [1, 3, 5]
 
 
-
 function reject(array, predicate) {
-    //
-    let newArr = []
-    for (i = 0; i < array.length; i++){
-    
-    if (i % 2 !== 0){
-    newArr.push(i)
+    console.log(predicate);
+    let arr = [];
+      for(let i = 0; i < array.length; i++){ 
+      if(predicate(array[i]) % 2 === 0){arr.push(array[i]);}
+      }
+      return arr;
     }
-    }
-    return newArr;
-  }
---
+++ 7kyu_round_up_to_the_nearest_multiple_of_5/7kyu_round_up_to_the_nearest_multiple_of_five.js
@@ -0,0 +1,30 @@
+// Given an integer as input, can you round it to the next (meaning, "higher") 5?
+
+// Examples:
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+
+
+
+
+function roundToNext5(n){
+    // ...
+    if (n % 5 !== 0){
+     n += 1} else {return n}
+     if (n % 5 !== 0){
+     n += 1} else {return n}
+     if (n % 5 !== 0){
+     n += 1} else {return n}
+     if (n % 5 !== 0){
+     n += 1} else {return n}
+     return n
+     
+     }
